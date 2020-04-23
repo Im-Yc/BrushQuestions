6ab7adb0d1725152c8b5048c5399e0d83496f12f
liuxin <liuxin@worken.cn>
2020-04-23 20:13:06 +0800

    init

---
+++ src/main/java/com/yc/brushquestions/brushquestions/BrushquestionsApplication.java
@@ -0,0 +1,13 @@
+package com.yc.brushquestions.brushquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushquestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushquestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yc/brushquestions/brushquestions/BrushquestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.brushquestions.brushquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrushquestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
